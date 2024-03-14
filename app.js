e24552f659893f4f2f7744f116f7305b317118fd
ClaraZalazar <zalazarmariaclara@gmail.com>
2024-03-14 20:25:00 -0300

    subo proyecto con React y JS

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const connectDB = require('./config/db');
+
+const app = express();
+
+app
+  .use('/', require('./routes/index.routes'))
+  .use('/api/auth',require('./routes/auth.routes')) 
+  .use('/api/users',require('./routes/users.routes')) 
+  .use('/api/projects',require('./routes/projects.routes')) 
+  .use('/api/tasks',require('./routes/tasks.routes')) 
+
+connectDB();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  return res.status(err.status || 500).json({
+    ok : false,
+    msg : err.message
+    });
+});
+
+module.exports = app;
